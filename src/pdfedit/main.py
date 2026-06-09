7df3ff365664063c982a4f8082ded4b68495f4ed
rjwilliajx <rjwilliajx@users.noreply.github.com>
2026-06-09 16:41:09 +0000

    Add initial PySide6 window

--- src/pdfedit/main.py
+++ src/pdfedit/main.py
@@ -1,6 +1,27 @@
-def main():
-    print("PDFEdit starting...")
+import sys
 
+from PySide6.QtWidgets import QApplication, QMainWindow, QLabel
+
+class PDFEditWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("PDFEdit")
+        self.setMinimumSize(900, 700)
+
+        label = QLabel("PDFEdit")
+        label.setStyleSheet("font-size: 32px;")
+        label.setMargin(30)
+
+        self.setCentralWidget(label)
+
+def main():
+    app = QApplication(sys.argv)
+
+    window = PDFEditWindow()
+    window.show()
+
+    sys.exit(app.exec())
 
 if __name__ == "__main__":
     main()
